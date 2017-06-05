d4769db44b438acfff579ab9fc92c51cdcb85019
Maarten Waegeman <maarten.waegeman@skynet.be>
2017-06-05 12:24:51 +0200

    Replacing default page with a splash page

---
+++ resources/views/splash.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+
+		<title>Parallel - EU Silvermoon</title>
+		
+		<!-- Bootstrap -->
+	    <link rel="stylesheet" href="css/bootstrap.min.css">
+	    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	    <link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div id="splash">
+			<h1>Parallel Guild<small>EU Silvermoon</small></h1>
+		</div>
+		<div id="splash2">
+			<p>Website coming <a href="/new">soon</a>!</p>
+			<p>Looking for the <a href="http://www.parallelguild.com/forum">forum</a> in the mean time?</p>
+			<p>Or find us on <a href="http://www.wowprogress.com/guild/eu/silvermoon/Parallel">WowProgress</a> (5/10M NH, 3/3 HC ToV, 7/7M EN).</p>
+		</div>
+		<footer>
+			<p>
+				All Rights Reserved by Parallel &copy; 2017
+			</p>
+		</footer>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
